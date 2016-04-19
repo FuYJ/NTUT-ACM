7ac2a8ad742ae41e7f6c5a0e30e9282a60c3e523
102820021 <102820021wsx987@gmail.com>
2016-04-19 18:15:42 +0800

    Added files via upload

--- js/chatboard_script.js
+++ js/chatboard_script.js
@@ -12,120 +12,35 @@ $(document).ready(function(){
     $(".new_discuss_title_block").val('re:關於ptc20160413第D題的解法');
   });
 
-  $(".title_block").click(function(){
-    var index = $(this).index();
-    if(index == 0){
-      if($(".content_block:nth-child(2)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(2)").addClass("display_block");
-      }
-    }
-    else if(index == 2){
-      if($(".content_block:nth-child(4)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(4)").addClass("display_block");
-      }
-    }
-    else if(index == 4){
-      if($(".content_block:nth-child(6)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(6)").addClass("display_block");
-      }
-    }
-    else if(index == 6){
-      if($(".content_block:nth-child(8)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(8)").addClass("display_block");
-      }
-    }
-    else if(index == 8){
-      if($(".content_block:nth-child(10)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(10)").addClass("display_block");
-      }
-    }
-    else if(index == 10){
-      if($(".content_block:nth-child(12)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(12)").addClass("display_block");
-      }
-    }
-    else if(index == 12){
-      if($(".content_block:nth-child(14)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(14)").addClass("display_block");
-      }
-    }
-    else if(index == 14){
-      if($(".content_block:nth-child(16)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(16)").addClass("display_block");
-      }
-    }
-    else if(index == 16){
-      if($(".content_block:nth-child(18)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(18)").addClass("display_block");
-      }
-    }
-    else if(index == 18){
-      if($(".content_block:nth-child(20)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(20)").addClass("display_block");
-      }
-    }
-    else if(index == 20){
-      if($(".content_block:nth-child(22)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(22)").addClass("display_block");
-      }
-    }
-    else if(index == 22){
-      if($(".content_block:nth-child(24)").hasClass("display_block")){
-        $(".content_block").removeClass("display_block");
-      }
-      else{
-        $(".content_block").removeClass("display_block");
-        $(".content_block:nth-child(24)").addClass("display_block");
-      }
-    }
+  $(".new_discuss_content_word").bind("input propertychange",function(){
+    var n = $(".new_discuss_content_word").val().length;
+    $(".new_discuss_content_word_limit").text(140-n);
+  })
 
-/*    $('.content').keydown(function(){
-        var n = $(".content").val().length+1;
-        $('.new_discuss_content_word_limit').html(n);
-    });*/
+  $(".new_discuss_content_submmit").click(function(){
+//    $(".content_list .title_block:nth-child(23) div").text('QQ');
+    for( var index = 23; index > 2; index -= 2){
+      var index2 = index - 2;
+      var index3 = index + 1;
+      var index4 = index3 - 2;
+      $(".content_list .title_block:nth-child(" + index + ") .title_block_word").text($(".content_list .title_block:nth-child(" + index2 + ") .title_block_word").text());
+      $(".content_list .title_block:nth-child(" + index + ") .title_block_user").text($(".content_list .title_block:nth-child(" + index2 + ") .title_block_user").text());
+      $(".content_list .title_block:nth-child(" + index + ") .title_block_time").text($(".content_list .title_block:nth-child(" + index2 + ") .title_block_time").text());
+      $(".content_list .content_block:nth-child(" + index3 + ") .content_word").text($(".content_list .content_block:nth-child(" + index4 + ") .content_word").text());
+    }
+    $(".content_list .title_block:nth-child(1) .title_block_word").text($(".new_discuss_title_block").val());
+    $(".content_list .title_block:nth-child(1) .title_block_user").text($(".new_discuss_user_block").val());
+    $(".content_list .content_block:nth-child(2) .content_word").text($(".new_discuss_content_word").val());
+  });
+
+  $(".title_block").click(function(){
+    var index = $(this).index() + 2;
+    if($(".content_block:nth-child(" + index + ")").hasClass("display_block")){
+      $(".content_block").removeClass("display_block");
+    }
+    else{
+      $(".content_block").removeClass("display_block");
+      $(".content_block:nth-child(" + index +")").addClass("display_block");
+    }
   });
 });
